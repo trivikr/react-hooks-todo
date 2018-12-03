72262714eb9fcc5bc77b499c5298f495540cc6ef
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-12-02 20:26:47 -0800

    Remove connect from AddToDo

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import React, { useReducer } from "react";
+import Footer from "./Footer";
+import AddTodo from "../containers/AddTodo";
+import VisibleTodoList from "../containers/VisibleTodoList";
+import rootReducer from "../reducers";
 
-const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+const Context = React.createContext();
 
-export default App
+const App = () => {
+  const [state, dispatch] = useReducer(rootReducer, []);
+  return (
+    <Context.Provider value={dispatch}>
+      <div>
+        <AddTodo dispatch={dispatch} />
+        {/*<VisibleTodoList />
+        <Footer />*/}
+      </div>
+    </Context.Provider>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import rootReducer from './reducers'
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
 
-const store = createStore(rootReducer)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+render(<App />, document.getElementById("root"));
