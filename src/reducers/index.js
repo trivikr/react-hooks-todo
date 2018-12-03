f7d2790fe81ee1ce95dbe57e072b5f6f9359508c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-12-02 20:12:31 -0800

    Remove combinedReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
-import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
+import todos from "./todos";
+import visibilityFilter from "./visibilityFilter";
 
-export default combineReducers({
-  todos,
-  visibilityFilter
-})
+export default function(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  };
+}
