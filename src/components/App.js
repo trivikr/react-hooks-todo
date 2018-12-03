db6d4cca1e77725db5e138040175b6c3a0cfcd95
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-12-02 23:13:14 -0800

    Remove return from App

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,12 @@ import Footer from "./Footer";
 import AddTodo from "../containers/AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
 
-const App = () => {
-  return (
-    <div>
-      <AddTodo />
-      <VisibleTodoList />
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
 
 export default App;
