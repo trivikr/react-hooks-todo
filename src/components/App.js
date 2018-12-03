cf62f175b98966307050822de9a1b204dda7b48d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-12-02 21:58:31 -0800

    Move Store to index.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,14 @@ import React, { useReducer } from "react";
 import Footer from "./Footer";
 import AddTodo from "../containers/AddTodo";
 import VisibleTodoList from "../containers/VisibleTodoList";
-import rootReducer from "../reducers";
-import { VisibilityFilters } from "../actions";
-import Store from "../store/Store";
 
 const App = () => {
   return (
-    <Store
-      rootReducer={rootReducer}
-      initialValue={{
-        visibilityFilter: VisibilityFilters.SHOW_ALL,
-        todos: []
-      }}
-    >
-      <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-      </div>
-    </Store>
+    <div>
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+import rootReducer from "./reducers";
+import { VisibilityFilters } from "./actions";
+import Store from "./store/Store";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Store
+    rootReducer={rootReducer}
+    initialValue={{
+      visibilityFilter: VisibilityFilters.SHOW_ALL,
+      todos: []
+    }}
+  >
+    <App />
+  </Store>,
+  document.getElementById("root")
+);
