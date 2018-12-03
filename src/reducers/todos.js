e9f3a0a0cf9fac18b015d4faf5a70294910ef5dc
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2018-12-02 21:53:04 -0800

    Run prettier on all files

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,6 +1,6 @@
 const todos = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case "ADD_TODO":
       return [
         ...state,
         {
@@ -8,16 +8,15 @@ const todos = (state = [], action) => {
           text: action.text,
           completed: false
         }
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(todo =>
-        (todo.id === action.id)
-          ? {...todo, completed: !todo.completed}
-          : todo
-      )
+      ];
+    case "TOGGLE_TODO":
+      return state.map(
+        todo =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default todos
+export default todos;
